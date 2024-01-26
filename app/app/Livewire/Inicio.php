46441e651153aa17e84a070fd51502e018ea9977
Franco Lago <fran080j9@gmail.com>
2024-01-26 16:31:50 -0300

    Crawler Funcionando
    Agregar anotaciones

--- app/app/Livewire/Inicio.php
+++ app/app/Livewire/Inicio.php
@@ -18,8 +18,11 @@ class Inicio extends Component
     }
 
     public function search(){
-        $crawler = new HttpBrowser(HttpClient::create());
-        $crawler->request('GET', 'https://www.cotodigital3.com.ar/sitios/cdigi/browse?_dyncharset=utf-8&Dy=1&Ntt='.$this->product.'&Nty=1&Ntk=&siteScope=ok&_D%3AsiteScope=+&atg_store_searchInput='.$this->product.'&idSucursal=200&_D%3AidSucursal=+&search=Ir&_D%3Asearch=+&_DARGS=%2Fsitios%2Fcartridges%2FSearchBox%2FSearchBox.jsp');
+        $browser = new HttpBrowser(HttpClient::create()); 
+        $crawler = $browser->request('GET', 'https://www.cotodigital3.com.ar/sitios/cdigi/browse?_dyncharset=utf-8&Dy=1&Ntt='.$this->product.'&Nty=1&Ntk=&siteScope=ok&_D%3AsiteScope=+&atg_store_searchInput='.$this->product.'&idSucursal=200&_D%3AidSucursal=+&search=Ir&_D%3Asearch=+&_DARGS=%2Fsitios%2Fcartridges%2FSearchBox%2FSearchBox.jsp');
+        $prods = $crawler -> filter('.atg_store_newPrice') -> first() -> text();
+        $prodName = $crawler -> filter('.atg_store_productTitle') -> first() -> text();
+        dd($prodName);
     }
 
 }
