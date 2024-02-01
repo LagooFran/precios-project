e165ed93f24d10059a706c95e755beeb9776f5a1
Franco Lago <fran080j9@gmail.com>
2024-02-01 19:17:03 -0300

    1

--- app/app/Livewire/Inicio.php
+++ app/app/Livewire/Inicio.php
@@ -42,67 +42,61 @@ class Inicio extends Component
         $browser = new HttpBrowser(HttpClient::create());
 
         $prods = [];
-        $coto = [];
 
         //Coto
 
-        // $crawler = $browser->request('GET', 'https://www.cotodigital3.com.ar/sitios/cdigi/browse?_dyncharset=utf-8&Dy=1&Ntt=' . $this->product . '&Nty=1&Ntk=&siteScope=ok&_D%3AsiteScope=+&atg_store_searchInput=' . $this->product . '&idSucursal=200&_D%3AidSucursal=+&search=Ir&_D%3Asearch=+&_DARGS=%2Fsitios%2Fcartridges%2FSearchBox%2FSearchBox.jsp');
+        $crawler = $browser->request('GET', 'https://www.cotodigital3.com.ar/sitios/cdigi/browse?_dyncharset=utf-8&Dy=1&Ntt=' . $this->product . '&Nty=1&Ntk=&siteScope=ok&_D%3AsiteScope=+&atg_store_searchInput=' . $this->product . '&idSucursal=200&_D%3AidSucursal=+&search=Ir&_D%3Asearch=+&_DARGS=%2Fsitios%2Fcartridges%2FSearchBox%2FSearchBox.jsp');
 
-        // foreach ($crawler->filter("[class='clearfix']") as $prod) {
-        //     array_push($prods, $prod);
-        // }
-        // foreach ($crawler->filter("[class='clearfix first']") as $prod) {
-        //     array_push($prods, $prod);
-        // }
-        // foreach ($crawler->filter("[class='clearfix first_max']") as $prod) {
-        //     array_push($prods, $prod);
-        // }
+        foreach ($crawler->filter("[class='clearfix']") as $prod) {
+            array_push($prods, $prod);
+        }
+        foreach ($crawler->filter("[class='clearfix first']") as $prod) {
+            array_push($prods, $prod);
+        }
+        foreach ($crawler->filter("[class='clearfix first_max']") as $prod) {
+            array_push($prods, $prod);
+        }
 
 
-        // foreach ($prods as $prod) {
-        //     try {
-        //         $crawlerProd = new Crawler($prod);
-        //         $name = $crawlerProd->filter("[class='descrip_full']")->text();
+        foreach ($prods as $prod) {
+            try {
+                $crawlerProd = new Crawler($prod);
+                $name = $crawlerProd->filter("[class='descrip_full']")->text();
 
-        //         if (similar_text(strtolower($name), strtolower(str_replace(' ', '', $this->product))) >= strlen(str_replace(' ', '', $this->product)) - 1) {
-        //             $price = $crawlerProd->filter("[class='atg_store_newPrice']")->text();
-        //             $img = $crawlerProd->filter("[class='atg_store_productImage'] > img")->attr('src');
-        //             $discountText = $crawlerProd->filter("[class='info_discount']")->children()->text();
-        //             if ($discountText == $price) {
-        //                 $discount = 'No';
-        //                 $discountText = 'None';
-        //             } else {
-        //                 $discount = 'Yes';
-        //                 $discountText = strstr($discountText, 'OFERTA');
-        //             }
+                if (similar_text(strtolower($name), strtolower(str_replace(' ', '', $this->product))) >= strlen(str_replace(' ', '', $this->product)) - 1) {
+                    $price = $crawlerProd->filter("[class='atg_store_newPrice']")->text();
+                    $img = $crawlerProd->filter("[class='atg_store_productImage'] > img")->attr('src');
+                    $discountText = $crawlerProd->filter("[class='info_discount']")->children()->text();
+                    if ($discountText == $price) {
+                        $discount = 'No';
+                        $discountText = 'None';
+                    } else {
+                        $discount = 'Yes';
+                        $discountText = strstr($discountText, 'OFERTA');
+                    }
 
-        //             $newProd = ([
-        //                 "name" => $name,
-        //                 "price" => $price,
-        //                 "storeId" => 'coto',
-        //                 "img" => $img,
-        //                 "discount" => $discount,
-        //                 "discountText" => $discountText
-        //             ]);
+                    $newProd = ([
+                        "name" => $name,
+                        "price" => $price,
+                        "storeId" => 'coto',
+                        "img" => $img,
+                        "discount" => $discount,
+                        "discountText" => $discountText
+                    ]);
 
-        //             array_push($coto, $newProd);
-        //         }
-        //     } catch (Exception $e) {
-        //     }
-        // }
+                    array_push($this->productsAnswer, $newProd);
+                }
+            } catch (Exception $e) {
+            }
+        }
 
         $prods = [];
-        $carrefour = [];
 
         // carrefour
 
 
-        // merge all products and sort them by price
+   
 
-        array_push($this->productsAnswer, $coto);
-        array_push($this->productsAnswer, $carrefour);
-
-        // show more button functionality
 
         if (count($this->productsAnswer) > 5) {
             $this->moreThan5 = true;
